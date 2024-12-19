b387c57e454702a9301fcfce21b7699595ce811e
Naveen Baskaran <naveenbaskaran@Naveens-MacBook-Air.local>
2024-12-19 09:36:39 +0530

    transfer list init commit

---
+++ transfer-list/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: { path: path.resolve(__dirname, 'dist') },
+    module: {
+        rules: [
+            {
+                test: /.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    }
+                }
+            },
+            {
+                test: /.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            module: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    devServer: {
+        host: 'localhost',
+        historyApiFallback: true,
+        open: true
+    }
+}
