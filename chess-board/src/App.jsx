321d7ed8d51915a3e356fb420b5ab9b29770035f
naveen-baskaran-3011 <naveenvelambas@gmail.com>
2024-12-20 23:30:40 +0530

    chess board

---
+++ chess-board/src/App.jsx
@@ -0,0 +1,107 @@
+import { useRef, useState } from "react";
+import './App.css';
+
+const chess_array = JSON.stringify(Array(8).fill(Array(8).fill(0)));
+
+function ltrDiagonal(rowIdx, columnIdx) {
+    const ltrDiagonalSet = new Set();
+    let [dummyRow, dummyColumn] = [rowIdx, columnIdx];
+    while(![dummyRow, dummyColumn].includes(-1)) {
+        ltrDiagonalSet.add(`[${dummyRow},${dummyColumn}]`);
+        --dummyRow;
+        --dummyColumn;
+    }
+
+    dummyRow = rowIdx;
+    dummyColumn = columnIdx;
+    while(Math.max(dummyRow, dummyColumn) < 8) {
+        ltrDiagonalSet.add(`[${dummyRow},${dummyColumn}]`);
+        ++dummyRow;
+        ++dummyColumn;
+    }
+
+    return ltrDiagonalSet;
+}
+
+function rtlDiagonal(rowIdx, columnIdx) {
+    const rtlDiagonalSet = new Set();
+    let [dummyRow, dummyColumn] = [rowIdx, columnIdx];
+    while(![dummyRow, dummyColumn].includes(-1) && Math.max(dummyRow, dummyColumn) < 8) {
+        rtlDiagonalSet.add(`[${dummyRow},${dummyColumn}]`);
+        --dummyRow;
+        ++dummyColumn;
+    }
+
+    dummyRow = rowIdx;
+    dummyColumn = columnIdx;
+    while(![dummyRow, dummyColumn].includes(-1) && Math.max(dummyRow, dummyColumn) < 8) {
+        rtlDiagonalSet.add(`[${dummyRow},${dummyColumn}]`);
+        ++dummyRow;
+        --dummyColumn;
+    }
+
+    return rtlDiagonalSet;
+}
+
+function calculatePossibleDiagonals(rowIdx, columnIdx) {
+    const ltrDiagonalSet = ltrDiagonal(rowIdx, columnIdx);
+    const rtlDiagonalSet = rtlDiagonal(rowIdx, columnIdx);
+    const calculatedPositionsSet = new Set([...ltrDiagonalSet, ...rtlDiagonalSet]);
+    console.log('ltrDiagonalSet', ltrDiagonalSet, 'rtlDiagonalSet', rtlDiagonalSet);
+    console.log('calculatedPositionsSet', calculatedPositionsSet)
+
+    const newChessBoard = JSON.parse(chess_array);
+    newChessBoard.forEach((row, rowId) => {
+        return row.forEach((column, columnId) => {
+            if(calculatedPositionsSet.has(`[${rowId},${columnId}]`)) {
+                newChessBoard[`${columnId}`][rowId] = 'diagonal';
+            }
+        })
+    });
+    newChessBoard[`${columnIdx}`][`${rowIdx}`] = 'selected'
+    return newChessBoard;
+}
+
+export default function App() {
+    const colorRef = useRef('white');
+    const [chessBoard, setChessBoard] = useState(JSON.parse(chess_array));
+    const clickHandler = (rowIdx, columnIdx) => {
+        setChessBoard([...calculatePossibleDiagonals(rowIdx, columnIdx)]);
+    };
+    return <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        border: '1px solid',
+        width: 'min(90vw,100vh - 160px)',
+        height: 'min(90vw,100vh - 160px)'
+    }}>
+        {chessBoard.map((row, rowIdx) => {
+            colorRef.current = colorRef.current === 'white' ? 'black' : 'white';
+
+            return (
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        flex: 1
+                    }}
+                    className="column"
+                    key={rowIdx}>
+                    {row.map && row.map((column, colIdx) => {
+                        colorRef.current = colorRef.current === 'white' ? 'black' : 'white';
+
+                        return (
+                        <div
+                            className={`row ${chessBoard[`${colIdx}`][`${rowIdx}`]}`}
+                            style={{
+                                backgroundColor: colorRef.current,
+                                flex: 1
+                            }}
+                            key={colIdx}
+                            onClick={() => clickHandler(rowIdx, colIdx)}></div>)
+                    })}
+                </div>
+            )
+        })}
+    </div>
+}
